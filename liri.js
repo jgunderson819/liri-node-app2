fe73466a207a5fec80c51a0049e95dfe15e3a0d6
Jeff Gunderson <jgunderson819@gmail.com>
2020-02-16 14:34:22 -0600

    Getting closer on the liri file.

--- liri.js
+++ liri.js
@@ -53,4 +53,48 @@ axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=co
 })}}
 
 
-function(spot)
+function(spot){
+    if artist="" { console.log("")}
+    else{
+    // Then run a request with axios to the Bands in Town API with the artist specified
+    axios.get(`"https://api.spotify.com/v1/artists" + artist + "/events?app_id=codingbootcamp"`).then(
+      function(response) {
+        console.log("Name of Venue " + response.data.venue);
+        console.log("Venue Location is " + response.data.address);
+        console.log("Date of the Event "+ response.data.date)
+      })
+      .catch(function(error) {
+        errorfuntion
+    })}}
+
+    function(mdb){
+        if movie="" { console.log("")}
+        else{
+            axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+                function(response) {
+                  console.log("Title: " + response.data.title);
+                  console.log("Year: " + response.data.Year);
+                  console.log("The movie's rating is: " + response.data.imdbRating);
+                  console.log("The movie's rating is: " + response.data.Ratings.Source="Rotten Tomatoes".Value);
+                  console.log("Country: " + response.data.Country);
+                  console.log("Language: " + response.data.Language);
+                  console.log("Plot: " + response.data.Plot);
+                  console.log("Actors: " + response.data.Actors);
+                })
+          .catch(function(error) {
+            errorfuntion
+        })}}
+
+        function(do-what-it-says){
+            if dowhat="" { console.log("")}
+            else{
+            // Then run a request with axios to the Bands in Town API with the artist specified
+            axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`).then(
+              function(response) {
+                console.log("Name of Venue " + response.data.venue);
+                console.log("Venue Location is " + response.data.address);
+                console.log("Date of the Event "+ response.data.date)
+              })
+              .catch(function(error) {
+                errorfuntion
+            })}}
