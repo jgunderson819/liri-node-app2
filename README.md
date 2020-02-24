91ae770dc2584e8849749d80e5d348d58f2436f8
Jeff Gunderson <jgunderson819@gmail.com>
2020-02-23 19:52:56 -0600

    have a complete file, just have to fix some errors.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # liri-node-app2
 liri-node-app
+
+You can get information on movies, concerts, or songs very easily from this app all from the command line.  Just input "node liri.js "concert-this","spotify-this-song", or "movie-this"" all from the command line.
--- liri.js
+++ liri.js
@@ -1,10 +1,18 @@
-requestAnimationFrame("dotenv").config();
+require("dotenv").config();
 
 var keys = require("./keys.js");
 var axios = require("axios");
-var spotify = new spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+var pry = require("Pryjs")
+var Spotify = require('node-spotify-api');
+
 var fs = require("fs");
-var moment = require(`moment`)
+var moment = require(`moment`);
+var inquirer = require("inquirer");
+var logger = fs.createWriteStream(`log.txt`,{flags:`a`});
+
+// create timestamp
+let timestamp = moment().format(`MM/DD/YYYY h:mm:ss a`)
 var artist = "";
 var concert="";
 var song="";
@@ -12,89 +20,198 @@ var movie="";
 var dowhat="";
 var variable =process.argv[3]
 var statement=process.arv[2]
-var errorfunction= function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
+// Create a variable to handle the full search argument
+let argument = "";
+getArgument();
+
+//Create timestamp each time the application is run.
+logger.append(`-------------------------\n` + timestamp + statement + `,`+argument + `\n`);
+
+  
+
+// We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (statement) {
+    case "concert-this":
+      concertThis(variable);
+      break;
+    
+    case "spotify-this-song":
+      spotifySong(variable);
+      break;
+    
+    case "movie-this":
+      movieThis(variable);
+      break;
+    
+    case "do-what-it-says":
+      doWhat(variable);
+      break;
     }
-    console.log(error.config);
-  }
+    
+// Grab argument from process.argv
+
+function getArgument(){
+
+    // grab the command line arguments
+    let args = process.argv;
+    // loop through the arguments starting at the 3rd index
+    for (let i=3; i<args.length; i++) {
+        if (i>3 && i<args.length) {argument = argument + " " + args[i]
+    } else argument += args[i];
+    }
+}
 
 
 
-
-
-
-function(concert-this){
-if artist="" { console.log("")}
-else{
+// concertThis function
+function concertThis(variable){
+;
 // Then run a request with axios to the Bands in Town API with the artist specified
 axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`).then(
   function(response) {
-    console.log("Name of Venue " + response.data.venue);
-    console.log("Venue Location is " + response.data.address);
-    console.log("Date of the Event "+ response.data.date)
-  })
+      // Grab the artist name and log it to the console.
+let artistName = response.data[0].artist.name 
+console.log("Upcoming concerts for ${artistName}:")
+console.log(response)
+// Loop through the response data and log the city, region, country, venue, and date for each result found
+for (let i=0; i < response.data.length; i++) {
+    let city = response.data[i].venue.city;
+    let region = response.data[i].venue.country;
+    let venue = response.data[i].venue.name;
+    let date = ""
+    date = moment(response.data[i].datetime).format(`MM/DD/YYYY`);
+    console.log(i);
+    console.log(`${city},${region},${country}, at ${venue}on ${date}` + "\n");
+}}) 
+
   .catch(function(error) {
-    errorfuntion
-})}}
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    }
+  
 
+  )}
 
-function(spot){
-    if artist="" { console.log("")}
+//spotify-this-song
+
+function spotifySong(variable){
+    if (argument="") { argument = "The Sign"}
     else{
     // Then run a request with axios to the Bands in Town API with the artist specified
-    axios.get(`"https://api.spotify.com/v1/artists" + artist + "/events?app_id=codingbootcamp"`).then(
-      function(response) {
-        console.log("Name of Venue " + response.data.venue);
-        console.log("Venue Location is " + response.data.address);
-        console.log("Date of the Event "+ response.data.date)
-      })
-      .catch(function(error) {
-        errorfuntion
-    })}}
-
-    function(mdb){
-        if movie="" { console.log("")}
+    spotify.search({ type: 'track', query: argument }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }}
+       .then (function(response){
+           let items = response.track.items;
+           for (i=0; i< items.length; i++);{
+               let artists = items[i].artists[0].name;
+               let song=items[i].name;
+               let preview = "";
+               if (items[i].preview_Url == null) { preview ="No preview link available"} else {
+                   preview = items[i].preview_Url;
+                   }
+                   let album = items[i].album.name
+                   console.log(i)
+                   console.log(`artist(s): `+ artists + "\n");
+                   console.log(`Song Name: ` + song);
+                   console.log(`Preview Song: ` + preview);
+                   console.log(`Album: ` + album)
+                   logger.write(`-----------------------` + "\n");
+                   logger.write(i + "\n");
+                   logger.write(`Artist(s)`+ artists + "\n");
+                   logger.write("Song name: " + song + `\n`);
+                   logger.write(`Preview Song: ` + preview + "\n");
+                   logger.write('Album: ' + album)
+           }
+       })
+      .catch(function(err) {console.log(err)}) 
+      )}}
+//  movie-this
+    function movieThis(variable){
+        if (variable="") { variable = "Mr. Nobody" }
         else{
-            axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+            axios.get("http://www.omdbapi.com/?t=" + variable + "&apikey=trilogy").then(
                 function(response) {
                   console.log("Title: " + response.data.title);
                   console.log("Year: " + response.data.Year);
                   console.log("The movie's rating is: " + response.data.imdbRating);
-                  console.log("The movie's rating is: " + response.data.Ratings.Source="Rotten Tomatoes".Value);
+                  console.log("The movie's rating is: "+ response.data.Ratings[1].Value);
                   console.log("Country: " + response.data.Country);
                   console.log("Language: " + response.data.Language);
                   console.log("Plot: " + response.data.Plot);
                   console.log("Actors: " + response.data.Actors);
+                  logger.write(`------------------------------`);
+                  logger.write("Title: " + response.data.title);
+                  logger.write("Year: " + response.data.Year);
+                  logger.write("The movie's rating is: " + response.data.imdbRating);
+                  logger.write("The movie's rating is: "+ response.data.Ratings[1].Value);
+                  logger.write("Country: " + response.data.Country);
+                  logger.write("Language: " + response.data.Language);
+                  logger.write("Plot: " + response.data.Plot); 
+                  logger.write("Actors: " + response.data.Actors)
                 })
-          .catch(function(error) {
-            errorfuntion
-        })}}
+          .catch(function(error) {if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+          }
+      
+    
+   , 
+)}
+        // do-what-it-says
+        function doWhat(){
+             // This block of code will read from the "random.txt" file.
+    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+    // The code will store the contents of the reading inside the variable "data"
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
-        function(do-what-it-says){
-            if dowhat="" { console.log("")}
-            else{
-            // Then run a request with axios to the Bands in Town API with the artist specified
-            axios.get(`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`).then(
-              function(response) {
-                console.log("Name of Venue " + response.data.venue);
-                console.log("Venue Location is " + response.data.address);
-                console.log("Date of the Event "+ response.data.date)
-              })
-              .catch(function(error) {
-                errorfuntion
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        let action = dataArr[0];
+        let query = dataArr[1];
+
+        if (action = "concert-this") {concertThis(query)}
+        else if (action = "spotify-this-song") {spotifySong(query)}
+        else if (action = "movie-this") {movieThis(query)}
+        else {console.log("I don't know what to do????????")}
+
             })}}
